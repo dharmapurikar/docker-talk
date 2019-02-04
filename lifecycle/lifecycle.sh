9918702aaa77d424690035edd8f416839df29ffd
Sachin Dharmapurikar <sachin@dharmapurikar.in>
2019-02-04 20:50:15 +0530

    Updated the text

--- lifecycle/lifecycle.sh
+++ lifecycle/lifecycle.sh
@@ -1,5 +1,5 @@
 docker -v
 t0=$(date "+%Y-%m-%dT%H:%M:%S")
-docker run --name=ephemeral -t dharmapurikar/cowsay 'I am ephemeral'
+docker run --name=ephemeral -t dharmapurikar/cowsay 'Hello Kodelounge!'
 t1=$(date "+%Y-%m-%dT%H:%M:%S")
 docker events --since $t0 --until $t1
--- lifecycle/lifecycle_disappear.sh
+++ lifecycle/lifecycle_disappear.sh
@@ -1,5 +1,5 @@
 docker -v
 t3=$(date "+%Y-%m-%dT%H:%M:%S")
-docker run --rm -t dharmapurikar/cowsay 'I am ephemeral'
+docker run --rm -t dharmapurikar/cowsay 'Hello Kodelounge!'
 t4=$(date "+%Y-%m-%dT%H:%M:%S")
 docker events --since $t3 --until $t4
